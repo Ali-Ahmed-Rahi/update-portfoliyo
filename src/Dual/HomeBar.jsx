b6602947906a4feea1a2097a7a963d4f385ac7dd
rahiji9 <aliahmedrahi007@gmail.com>
2026-02-27 23:51:51 +0600

    about project

--- src/Dual/HomeBar.jsx
+++ src/Dual/HomeBar.jsx
@@ -1,6 +1,7 @@
 import AboutMe from "../components/AboutMe";
 import Banner from "../components/Banner";
 import Contact from "../components/Contact";
+import Projec from "../components/projec";
 import Projects from "../components/Project";
 import Skills from "../components/Skills";
 
@@ -12,7 +13,8 @@ const HomeBar = () => {
       <Banner></Banner>
       <AboutMe></AboutMe>
       <Skills></Skills>
-      <Projects></Projects>
+      {/* <Projects></Projects> */}
+      <Projec></Projec>
       <Contact></Contact>
       
     </div>
---
+++ src/components/projec.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Projec = () => {
+  return (
+    <div>
+      <div className="space-y-5">
+        <h2 className="text-md font-work text-yellow-500 space-x-4">
+          Own Projects
+        </h2>
+        <h2 className="uppercase font-playfair flex gap-[10px] text-2xl">
+          Have <span>Some</span> Project's
+        </h2>
+      </div>
+      <div className='mt-10'>
+        <h2 className='text-2xl text-yellow-400 font-bold'>Project Are going on <span className='animate-pulse'>.....</span></h2>
+      </div>
+    </div>
+  );
+};
+
+export default Projec;
